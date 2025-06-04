7f40267254631a0bfc844110485533ecab0715b7
Methawee101 <pray.methawee@gmail.com>
2025-06-04 23:33:15 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav class ="bg-pink-200 w-full">
+        <div class = "flex flex-wrap items-center justify-between p-4">
+          <a class = "items-center text-4xl self-center ">Portfolio</a>
+          
+          <div  id = "navbar-default">
+            <ul class = "flex text-3xl p-0 mt-4 bg-pink-200   flex-row space-x-8 space-x-reverse mt-0 border-0 ">
+              <li>
+                <a href="#" class ="bg-transparent md:text-blue-700 " aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="#" class ="bg-transparent md:text-blue-700 p-4">About</a>
+              </li>
+              <li>
+                <a href="#" class ="bg-transparent md:text-blue-700 p-4">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
